789223fb8cb81e2af97c74ca0f026bad2b0df03b
Seleste <104727784+smillkaa@users.noreply.github.com>
2022-08-20 14:25:46 -0700

    displays notes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,5 +1,8 @@
 const router = require('express').Router()
 const fs = require('fs')
+const requestID = require('express-request-id')
+
+router.use(requestID())
 
 router.get('/notes', (req, res) => {
   fs.readFile('./db/db.json', (error, data) => {
@@ -9,10 +12,5 @@ router.get('/notes', (req, res) => {
     res.json(JSON.parse(data))
   })
 })
-
-
-// router.post('/api/notes', (req, res) => {
-//     // hould receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
-// })
 
 module.exports = router
